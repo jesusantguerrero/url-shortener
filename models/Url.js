456f59d5a1d534507664e092843b002823b561c6
jesusantguerrero <me@jesusantguerrero.com>
2017-12-19 03:51:28 -0400

    short url ready

--- models/Url.js
+++ models/Url.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Url = mongoose.model('Url',new mongoose.Schema({
   original_url: String,
-  shorten_url: mongoose.Schema.Types.ObjectId
+  shorten_url: mongoose.Schema.Types.String
 }))
 
 module.exports = Url
--- server.js
+++ server.js
@@ -20,16 +20,26 @@ app.get('/', (req, res)=> {
 app.get('/api/*',(req, res) => {
   let url = req.originalUrl.slice(5);
   if (/^https?:\/\/\w*\.\w*/.test(url.toString())){
-    const data = addUrl(url).then((data) => {
-      res.json(data);
+    addUrl(url).then((data) => {
+      const datas = {
+        original_url: data.original_url,
+        shorten_url: `${req.baseUrl}${data._id.toString().slice(-6)}`
+      };
+      res.json(datas);
     })
   } else {
     res.end(`${url} this is an invalid url`);
   }
 });
 
-app.get(':short', (req, res) => {
-  UrlModel.$where('_id')
+app.get('/:short', (req, res) => {
+  getUrl(req.params.short).then((url) => {
+    if (url) {
+      res.redirect(url.original_url)
+    } else {
+      res.end('no conocemos esa url')
+    }
+  })
 })
 
 var listener = app.listen(process.env.PORT || 3000, (res) => {
@@ -37,12 +47,22 @@ var listener = app.listen(process.env.PORT || 3000, (res) => {
 
 
 function addUrl(url) {
-
   return UrlModel.create({original_url: url}, ((err, newUrl)=> {
     if (err)
       console.log(err)
-    //const shorten_url = newUrl._id.slice(-6);
-    //UrlModel.update({_id: newUrl._id}, {shorten_url: shorten_url})
-    return newUrl
+    const shorten_url = newUrl._id.toString().slice(-6);
+    return UrlModel.update({_id: newUrl._id},{ shorten_url: shorten_url},(err, data) => {
+      if (err)
+        console.log(err)
+      return data
+    })
   }))
+}
+
+function getUrl(short) {
+  return UrlModel.findOne({shorten_url: short}, (err, res) => {
+    if (err) 
+      return false
+    return res.original_url
+  })
 }
